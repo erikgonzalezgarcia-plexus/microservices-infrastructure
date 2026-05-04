ea41eecb628167a4602fd7eb5e15ae496c6ca352
Erik Gabriel Gonzalez Garcia <erik.gonzalezgarcia@plexus.es>
2026-05-04 13:18:16 +0200

    test(order): verify spotless plugin configuration

--- order/src/test/java/com/training/order/OrderApplicationTests.java
+++ order/src/test/java/com/training/order/OrderApplicationTests.java
@@ -1,13 +1,25 @@
 package com.training.order;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrderApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void pomContainsSpotlessPluginConfiguration() throws IOException {
+        String pomContent = Files.readString(Path.of("pom.xml"));
+
+        assertTrue(pomContent.contains("spotless-maven-plugin"));
+        assertTrue(pomContent.contains("<goal>check</goal>"));
+    }
 }
